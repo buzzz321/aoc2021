24a245e9e5355c6d7addcae7baae4834476a1b73
Anders Olme <anders.olme@gmail.com>
2021-12-09 20:57:01 +0100

    Update main.zig
    More added to day 3 but we still have shifting problems..

--- aoc_3/src/main.zig
+++ aoc_3/src/main.zig
@@ -31,17 +31,32 @@ pub fn main() anyerror!void {
         if (char == '\n') {
             width = index - start;
             //std.debug.print("-> {s} w = {d}\n", .{ sensor_data[start..index], width });
-            tmp = try std.fmt.parseInt(u64, std.mem.trimRight(u8, sensor_data[start..index], "\n"), 0);
+            tmp = try std.fmt.parseInt(u64, std.mem.trimRight(u8, sensor_data[start..index], "\n"), 2);
             try list.append(tmp);
             start = index + 1;
         }
     }
     //std.debug.print("-> {s}\n", .{sensor_data[start..]});
-    tmp = try std.fmt.parseInt(u64, std.mem.trimRight(u8, sensor_data[start..], "\n"), 0);
+    std.debug.print("-> {d}\n", .{list.items[0]});
+    tmp = try std.fmt.parseInt(u64, std.mem.trimRight(u8, sensor_data[start..], "\n"), 2);
     try list.append(tmp);
 
-    for (list.items) |item| {
-        std.debug.print("=> {d:0>5}\n", .{item});
+    var index: u64 = 0;
+    var sensor_bit: u64 = 0;
+    while (index <= width) {
+        //for (list.items) |item| {
+        {
+            var item: u64 = list.items[0];
+            //std.debug.print("+> {d}\n", .{width - index});
+            sensor_bit = item & (@as(u64, 2) << @intCast(u6, (width - index)));
+            if (sensor_bit > 0) {
+                std.debug.print("=> {d}\n", .{1});
+            } else {
+                std.debug.print("=> {d}\n", .{0});
+            }
+            //std.debug.print("=> {d:0>5}\n", .{item});
+        }
+        index += 1;
     }
 
     //std.log.info("All your codebase are belong to us.", .{});
